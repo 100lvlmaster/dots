0dd4014105fb2711122231ac11dd3ffe7526b984
Navin Kodag <navinkodag@gmail.com>
2021-06-18 18:00:10 +0530

    9

--- restore_config.sh
+++ restore_config.sh
@@ -19,3 +19,15 @@ cp -R ./wallpapers ~/
 
 # Set wallpaper
 nitrogen --set-scaled ./wallpapers/wallpaper.png
+
+
+# Install paru package manager
+
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+
+# Install all packages
+awk '{print $1}'  package_list.txt |  xargs paru -S

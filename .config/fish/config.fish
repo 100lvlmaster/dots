d128df8a3ca3055e15035e297a7d12db0724ee23
100lvlmaster <navin@mybytecode.com>
2021-12-05 16:09:49 +0530

    4

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 #   Exports
 export PATH="$PATH:/opt/flutter/bin"
 export CHROME_EXECUTABLE="/opt/google/chrome/chrome"
-
+export BROWSER=/usr/bin/brave
 # Go
 set -g GOPATH $HOME/go
 set -gx PATH $GOPATH/bin $PATH
@@ -10,5 +10,7 @@ set -gx PATH $GOPATH/bin $PATH
 alias open="xdg-open"
 alias cl="clear"
 alias r="ranger"
+alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
+
 
 starship init fish | source
